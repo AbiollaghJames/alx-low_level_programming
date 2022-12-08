d004391b912d6042c6fb7ee76a3620ebf5a068e6
AbiollaghJames <abiollajames@protonmail.com>
2022-12-08 09:14:39 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
-	puts(str);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
