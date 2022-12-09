74fe6c20daf8a708286e05bc315114969c91d23d
AbiollaghJames <abiollajames@protonmail.com>
2022-12-09 13:15:35 -0800

    Insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+	}
+	putchar("\n");
+}
