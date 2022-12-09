80ded3aeb1ebc73590d603f8b96c52cda7045352
AbiollaghJames <abiollajames@protonmail.com>
2022-12-09 14:30:49 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int abby;
+
+	abby = 0;
+
+	while (abby < 10)
+	{
+		putchar(abby);
+	}
+	putchar('\n');
+	return (0);
+}
