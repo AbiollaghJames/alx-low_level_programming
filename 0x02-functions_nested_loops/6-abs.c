a48b4063079112d45a749713fdeda0b6989daacd
AbiollaghJames <abiollajames@protonmail.com>
2022-12-13 10:55:28 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * int_abs - Compute absolute value of int
+ *
+ * Return: The absolute value
+ */
+int _abs(int)
+{
+	int abb;
+	
+	if (abb >= 0)
+	{
+		return (abb);
+	}
+	else 
+	{
+		return (-abb);
+	}
+}
