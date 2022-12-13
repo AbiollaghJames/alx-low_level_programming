ca58a83394d25ab5bbacbaafadfa0bf00f1ec24b
AbiollaghJames <abiollajames@protonmail.com>
2022-12-13 10:27:44 -0800

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * _isalpha - checks for alphabetical character
+ *
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if (c <= 'a' && c >= 'z' || c <= 'A' && c >= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
