2df613af2740381d5e8803468b77c80ee99cb42c
AbiollaghJames <abiollajames@protonmail.com>
2022-12-13 12:13:21 -0800

    98 battery street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
  * print_to_98 - prints natural numbers
+ * @n: parameter
  *
  * Return: 0
  */
 void print_to_98(int n)
 {
-	for (n = 0; n < 99; n++)
+	while (n < 99)
 	{
 		_putchar(n);
+		_putchar(',');
+		n++;
 	}
 }
