c2341e3dc387cd0110165213442fa2f7f4cac180
AbiollaghJames <abiollajames@protonmail.com>
2022-12-13 09:14:35 -0800

    Insomnia play

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * main -Entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char abb = 'a';
+
+	while (abb <= 'z')
+	{
+		_putchar(abb);
+	}
+	_putchar('\n');
+}
