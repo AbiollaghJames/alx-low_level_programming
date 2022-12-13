475f6a2af609736d8ab2301ac744dfdbdc22d9dd
AbiollaghJames <abiollajames@protonmail.com>
2022-12-13 10:19:09 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * islower - checks if a character is lpowercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
