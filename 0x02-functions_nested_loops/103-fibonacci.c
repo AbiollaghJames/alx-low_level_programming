790b3ff8d642c478be60e8728b35c2f1d80eda4a
AbiollaghJames <abiollajames@protonmail.com>
2022-12-14 11:24:57 -0800

    Even liber abacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2, sum = k;
+
+	while (k + j < 4000000)
+	{
+		k += j;
+
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+		++i;
+	}
+	printf("%ld'n", sum);
+	return (0);
+}
