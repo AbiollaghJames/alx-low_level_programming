3ad961463cf22233131901a7b53014e91f013a1e
AbiollaghJames <abiollajames@protonmail.com>
2022-12-14 10:20:26 -0800

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x, y = 0;
+	while (x < 1024)
+	{
+		if ((y % 3 == 0) || (y % 5 == 0))
+		{
+			y += x;
+		}
+		x++;
+	}
+	printf("%d\n", y);
+	return (0);
+}
