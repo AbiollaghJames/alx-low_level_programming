5bba24baeef47079ac275b569e0b4928d4aa6fd7
AbiollaghJames <abiollajames@protonmail.com>
2022-12-14 10:29:55 -0800

    50 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdiio.h>
+/**
+ * main - Entry point
+ *
+ * Return; 0
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%d", j);
+		else if (i == 1)
+			printf(", %d", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %d", k);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
