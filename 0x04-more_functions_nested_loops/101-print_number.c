777aed6ddab3d764e042bc10de9b844c424c5471
AbiollaghJames <abiollajames@protonmail.com>
2022-12-15 09:47:37 -0800

    print int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,9 +7,14 @@
  */
 void print_number(int n)
 {
-	for (n = 0; n <= 9; n++)
+	unsigned int number = n;
+
+	if (n < 0)
 	{
-		_putchar((n % 10) + '0');
+		_putchar('-');
+		number = -number;
 	}
-	_putchar('\n');
+	if ((number / 10) > 0)
+		print_number(number / 10);
+	_putchar((number % 10) + '0');
 }
