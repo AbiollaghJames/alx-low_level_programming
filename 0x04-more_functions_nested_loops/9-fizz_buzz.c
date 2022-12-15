e0decd7c01f2a20e251c5ea28014c07e0c406564
AbiollaghJames <abiollajames@protonmail.com>
2022-12-15 06:01:52 -0800

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((x % 3 == 0) || (x % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf(x);
+		}
+		printf(" ");
+	}
+	return (0);
+}
