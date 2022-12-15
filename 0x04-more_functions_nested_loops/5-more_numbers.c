6b4affbf9707a1696d8a52e49ade232bdabebfe3
AbiollaghJames <abiollajames@protonmail.com>
2022-12-15 02:34:41 -0800

    Numbers are the universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,12 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 1; a <= 10; a++)
+	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			if (b >= 10)
-			{
-				_putchar('1');
-				_putchar(b % 10 + '0');
-			}
-			_putchar('\n');
+			_putchar(b + '0');
+		}
+		_putchar('\n');
 	}
 }
