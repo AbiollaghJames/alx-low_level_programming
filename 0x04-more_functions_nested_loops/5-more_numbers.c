8b07bdebcd1d1e3b29adb2fc879c03e9f6fd314f
AbiollaghJames <abiollajames@protonmail.com>
2022-12-15 08:49:29 -0800

    Numbers the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,8 +12,11 @@ void more_numbers(void)
 	{
 		for (b = 0; b < 15; b++)
 		{
-			_putchar(b + '0');
+			if (a > 9)
+			{
+			_putchar((a / 10) + '0');
+			}
+			_putchar((a % 10) + '0');
 		}
-		_putchar('\n');
 	}
 }
