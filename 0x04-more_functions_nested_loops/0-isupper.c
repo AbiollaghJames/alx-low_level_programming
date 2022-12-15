37a34e9aa6be362966844e6a0cfa1e6fcf1e6ccc
AbiollaghJames <abiollajames@protonmail.com>
2022-12-15 01:26:01 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _usSupper - Checks for uppercase
+ *@c: parameter
+ *
+ * Return: 0
+ */
+int _ispper(int c)
+{
+	if (c <= 'z' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
