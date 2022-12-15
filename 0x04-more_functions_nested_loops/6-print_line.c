f4dff12c7c33206efa8805caa25119d216cce249
AbiollaghJames <abiollajames@protonmail.com>
2022-12-15 03:48:22 -0800

    shortest distance

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,19 +7,9 @@
  */
 void print_line(int n)
 {
-	int x;
-
-	for (x = 0; x <= n; x++)
+	while (n -- > 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar('_');
-			_putchar('\n');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
