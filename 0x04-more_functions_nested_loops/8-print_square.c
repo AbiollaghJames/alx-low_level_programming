4077f0adb87a3758d807a14fe6e33c4c48b295ce
AbiollaghJames <abiollajames@protonmail.com>
2022-12-15 04:44:08 -0800

    sunshine on every square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - Prints a square
+ * @size: parameter
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int h, w;
+
+	if (size > 0)
+	{
+		for (h = 0; h <= size; h++)
+		{
+			for(w = 0; w <= size; w++)
+			{
+				_putchar('#');
+			}
+			if (h == size - 1)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
