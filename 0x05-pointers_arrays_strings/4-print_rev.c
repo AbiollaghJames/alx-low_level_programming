746d743c951164fcb5be1c74d261e700f21eaf22
AbiollaghJames <abiollajames@protonmail.com>
2022-12-19 06:50:04 -0800

    count

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,10 @@ void print_rev(char *s)
 	int count = 0;
 
 	while (s[count] != '\0')
-	{
 		count++;
-	}
+
 	for (count = count - 1; count >= 0; count--)
-	{
 		_putchar(s[count]);
-	}
+
 	_putchar('\n');
 }
