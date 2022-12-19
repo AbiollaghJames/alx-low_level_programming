47129899fa60b24e761e6af234c827210217a6a1
AbiollaghJames <abiollajames@protonmail.com>
2022-12-19 11:59:52 -0800

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,19 +7,17 @@
  */
 void print_array(int *a, int n)
 {
-	int i, len1, len2;
-	char tmp;
+	int x;
 
-	while (s[len1] != '\0')
-		len1++;
-
-	len2 = len1 - 1;
-
-	for (i = 0; i < len1; i++)
+	for (x = 0; x < n; x++)
 	{
-		tmp = s[i];
-		s[i] = s[len2];
-		s[len2] = tmp;
-		len2 -= 1;
+		printf("%d", a[x]);
+
+		if (x != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
 	}
+		printf("\n");
 }
