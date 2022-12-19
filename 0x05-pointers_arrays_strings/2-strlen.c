e44f934999eaaf9113ffaa553ef08b57502fd47a
AbiollaghJames <abiollajames@protonmail.com>
2022-12-19 04:36:59 -0800

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - length of string
+ * @s: parameter
+ * Return 0
+ */
+int _strlen(char *s)
+{
+	int str;
+
+	for (str = 0; s[str] != '\0'; str++)
+	{
+		return (str);
+	}
+}
