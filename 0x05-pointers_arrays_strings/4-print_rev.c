c3285f4220b1f34432ae36bbdf2847701d49d202
AbiollaghJames <abiollajames@protonmail.com>
2022-12-19 06:26:40 -0800

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@ void print_rev(char *s)
 {
 	int count = 0;
 
-	while (s[count] >= 0)
+	while (s[count] != '\0')
 	{
-		if (s[count] == '\0')
-			break;
 		count++;
 	}
-	for (count--; count >= 0; count--)
+	for (count = count - 1; count >= 0; count--)
+	{
 		_putchar(s[count]);
+	}
 	_putchar('\n');
 }
