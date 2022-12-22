6e6b12885db29604469bd17f7f8dde31d4d40ea3
AbiollaghJames <abiollajames@protonmail.com>
2022-12-22 03:21:17 -0800

    print num

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,14 +5,42 @@
  */
 void print_number(int n)
 {
-	unsigned int x = n;
+	int i, j, dig, digs, pow;
+	unsigned int tmp, numchar, num;
+
+	dig = 0;
 
 	if (n < 0)
 	{
-		_putchar('-')
-			x = -x;
+		_putchar('-');
+		tmp = -n;
 	}
-	if ((x / 10) > 0)
-		print_numbere(x / 10);
-	_putchar((x % 10) + '0');
+	else
+	{
+		tmp = n;
+	}
+	num = tmp;
+
+	while (num >= 10)
+	{
+		num = num / 10;
+		dig++;
+	}
+	digs = dig + 1;
+	pow = 1;
+	i = 1;
+
+	while (i < digs)
+	{
+		pow = pow * 10;
+		i++;
+	}
+	j = pow;
+
+	while (j >= 1)
+	{
+		numchar = (tmp / j) % 10;
+		_putchar(numchar + '0');
+		j = j / 10;
+	}
 }
