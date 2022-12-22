c945334c5e8a2d01c05fbdda30f095ffb2de5b07
AbiollaghJames <abiollajames@protonmail.com>
2022-12-22 01:29:20 -0800

    capitalize words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,28 +4,29 @@
  * @str: parameter
  * Return: capitalized words
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-	int i = 0;
+	int x, y;
+	int alph = 32;
+	int sep[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
 
-	while (str[++i])
+	for (x = 0; str[x] != '\0'; x++)
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}')
-			str[i] -= 32;
+		if (str[x] >= 'a' && str[x] <= 'z')
+		{
+			str[x] = str[x] - alph;
+		}
+
+		alph = 0;
+
+		for (y = 0; y <= 12; y++)
+		{
+			if (str[x] == sep[y])
+			{
+				y = 12;
+				alph = 32;
+			}
+		}
 	}
 	return (str);
 }
