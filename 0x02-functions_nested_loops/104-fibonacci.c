7d47c971a28e201a766f1ac12a5f667caf21c707
AbiollaghJames <abiollajames@protonmail.com>
2022-12-27 12:15:29 -0800

    98 fib

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int count 3;
+	int x = 1, y = 2;
+	long int res = x + y;
+
+	printf("%lu, ", x);
+	printf("%lu, ", y);
+
+	while (count <= 98)
+	{
+		if (count == 98)
+		{
+			printf("%lu \n", res);
+		}
+		else
+		{
+			printf("%lu ", res);
+		}
+
+		x = y;
+		y = res;
+
+		res = x + y;
+		count++;
+	}
+	return (0);
+}
