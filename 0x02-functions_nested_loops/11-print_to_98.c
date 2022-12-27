dcef643d21b8053d42bcad1f2b14086c77810e4e
AbiollaghJames <abiollajames@protonmail.com>
2022-12-27 11:29:45 -0800

    98 battery street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,36 +7,11 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	int x;
+
+	for (x = n; x <= 98; x++)
 	{
-		for (; n <= 98; n++)
-		{
-			if (n ==98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-	}
-	else
-	{
-		for (; n >= 98; n--)
-		{
-			if (n ==98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
+		printf("%d", x);
+		printf("\n");
 	}
 }
