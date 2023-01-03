845389f1ab55a42fd1b0e47203d263f43274825b
AbiollaghJames <abiollajames@protonmail.com>
2023-01-03 12:05:15 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - Fill memory with value
+ * @s: parameter 1
+ * @b: parameter 2
+ * @n: parameter 3
+ * Return: changed array with n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x = 0;
+
+	for (; x > 0; x++)
+	{
+		s[x] - b;
+		x--;
+	}
+	return (s);
+}
