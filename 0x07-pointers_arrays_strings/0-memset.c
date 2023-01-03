30ecc14c7af755836746e5c1ac6d822bddcdadda
AbiollaghJames <abiollajames@protonmail.com>
2023-01-03 12:09:08 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int x = 0;
+	unsigned int x = 0;
 
-	for (; x > 0; x++)
+	while (x < n)
 	{
-		s[x] - b;
-		x--;
+		*(s + x) = b;
+		x++;
 	}
 	return (s);
 }
