5ce3d6c8c263893915ea5c579a66e186ae60f01c
AbiollaghJames <abiollajames@protonmail.com>
2023-01-03 13:33:43 -0800

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,12 +8,10 @@ void print_chessboard(char (*a)[8])
 {
 	int x, y;
 
-	for (x = 0; x <= 7; x++)
+	for (x = 0; x < 8; x++)
 	{
-		for (y = 0; y <= 7; y++)
-		{
+		for (y = 0; y < 8; y++)
 			_putchar(a[x][y]);
-		}
 		_putchar('\n');
 	}
 }
