7d091655c3efe0ca41154bbec1e9014f4657578c
AbiollaghJames <abiollajames@protonmail.com>
2023-01-03 13:06:36 -0800

    double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - set value of pointer to char
+ * @s: paranmeter 1
+ * @to: parameter 2
+ * Return: 0 Success
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
