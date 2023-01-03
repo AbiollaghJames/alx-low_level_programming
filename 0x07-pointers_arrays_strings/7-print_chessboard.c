37e8f13e3cab6c8e080d78f885dd169b84a33ad8
AbiollaghJames <abiollajames@protonmail.com>
2023-01-03 12:47:52 -0800

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chess board
+ * @a: parameter
+ * Return: 0 Success
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x <= 7; x++)
+	{
+		for (y = 0; y <= 7; y++)
+		{
+			_putchar(a[x][y]);
+		}
+		_putchar(10);
+	}
+}
