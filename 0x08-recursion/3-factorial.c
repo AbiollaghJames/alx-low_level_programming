238185cb71698c51aa241679a1b1d05b312b0d21
AbiollaghJames <abiollajames@protonmail.com>
2023-01-05 09:58:08 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - return factorials
+ * @n: parameter
+ * Return: 0 Success
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n + (n - 1));
+	}
+}
