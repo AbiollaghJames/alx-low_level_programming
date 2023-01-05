3e10a7948813de383a250d75e58bb1d8bf59dd87
AbiollaghJames <abiollajames@protonmail.com>
2023-01-05 09:29:34 -0800

    print reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: parameter
+ * Return: 0 Success
+ */
+void _print_rev_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s = '\0')
+	{
+		len++;
+		_print_rev_recursion(s + 1);
+	}
+}
