7f4d249f759663cbad1382f72a28b0128e7a54ed
AbiollaghJames <abiollajames@protonmail.com>
2023-01-09 12:48:31 -0800

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - addition
+ * @argc: par 1
+ * @argv: par 2
+ * Return: 0 Success
+ */
+int main(int argc, char *argv)
+{
+	int res, num, x, y, z;
+
+	if (argc == 1)
+		printf("0\n");
+
+	for (x = 0; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+	}
+	for (z = 0; z < argc; z++)
+	{
+		num = atoi(argv[z]);
+		res += num;
+	}
+	printf("%d\n", res);
+	return (0);
+}
