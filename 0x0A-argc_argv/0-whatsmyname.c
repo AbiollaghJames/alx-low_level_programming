ac84fb1911f5569248de8f8f2cd899dbd77983ac
AbiollaghJames <abiollajames@protonmail.com>
2023-01-09 11:47:19 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints program name
+ * @argc: par 1
+ * @argv: par 2
+ * Return: 0 Success
+ */
+int main(int argc(unused), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
