3457e431c6b1a6c06e8a11098159be257910ed7f
AbiollaghJames <abiollajames@protonmail.com>
2023-01-09 12:13:16 -0800

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints number of args
+ * @argc: par 1
+ * @argv: par 2
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
