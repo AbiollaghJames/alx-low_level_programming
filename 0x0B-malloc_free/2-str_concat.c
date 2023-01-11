5b27c946d6de427e663cf78fbc58011326cace29
AbiollaghJames <abiollajames@protonmail.com>
2023-01-11 00:02:24 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,19 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _str_len - length of string
- * @str: par
- * Return: 0 Success
- */
-int _str_len(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	return (i + 1);
-}
-/**
  * str_concat - concatenates 2 strings
  * @s1: par 1
  * @s2: par 2
@@ -21,27 +8,29 @@ int _str_len(char *str)
  */
 char *str_concat(char *s1, char *s2)
 {
-	int s1_size, s2_size, i, j;
-	char *ptr;
+  char *newstr = NULL;
+  unsigned int i;
+  int n1, n2, count = 0;
 
-	if (s1 == NULL)
-		s1 = " ";
-	if (s2 == NULL)
-		s2 = " ";
-
-	s1_size = _str_len(s1);
-	s2_size = _str_len(s2);
-
-	ptr = (char *) malloc(((s1_size + s2_size) - 1) * sizeof(char));
-
-	if (ptr == NULL)
-		return (NULL);
-
-	for (i = 0; s1[i] != '\0'; i++)
-		ptr[i] = s1[i];
-	for (j = 0; s2[j] != '\0'; j++)
-		ptr[i] = s2[j];
-	ptr[i] = '\0';
-
-	return (ptr);
+  if (s1 == NULL)
+    s1 = "";
+  if (s2 == NULL)
+    s2 = "";
+  for (n1 = 0; s1[n1] != '\0'; n1++)
+    ;
+  for (n2 = 0; s2[n2] != '\0'; n2++)
+    ;
+  newstr = (char *)malloc((n1 + n2 + 1) * sizeof(char));
+  if (newstr == NULL)
+    {
+      return (NULL);
+    }
+  for (i = 0; s1[i] != '\0'; i++)
+    newstr[i] = s1[i];
+  for (; s2[count] != '\0'; i++)
+    {
+      newstr[i] = s2[count];
+      count++;
+    }
+  return (newstr);
 }
