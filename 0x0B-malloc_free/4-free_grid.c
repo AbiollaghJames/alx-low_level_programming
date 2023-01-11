46d2aa79a57cfc852e9a7791f77ae94716d433a4
AbiollaghJames <abiollajames@protonmail.com>
2023-01-11 11:54:08 -0800

    task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * free_grid - frees memory
  * @grid: par 1
@@ -8,9 +9,12 @@
  */
 void free_grid(int **grid, int height)
 {
-	int i;
+  int i, *p;
 
-	for (i = 0; i < height; i++)
-		free(grid[i]);
-	free(grid);
+  for (i = 0; i < height; i++)
+    {
+      p = grid[i];
+      free(p);
+    }
+  free(grid);
 }
