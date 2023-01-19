711f3884ded36e1ce58696eecc00e67d4e366f62
AbiollaghJames <abiollajames@protonmail.com>
2023-01-19 10:09:49 -0800

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,13 +12,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array && size && cmp)
-		return;
-
-	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		for (i = 0; i < size; i++)
 		{
-			return (1);
+			if (cmp(array[i]))
+				return (i);
 		}
 	}
 	return (-1);
