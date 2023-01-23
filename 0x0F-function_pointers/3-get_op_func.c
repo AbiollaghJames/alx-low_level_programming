599913a1eb805dbedb0366e8648321bbdfdca4ff
AbiollaghJames <abiollajames@protonmail.com>
2023-01-23 04:03:12 -0800

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,24 +4,24 @@
  * @s: operator given by user
  * Return: 0 Success
  */
-int (*get_op_func(char *s))(int, int)
-{
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-
-	int i = 0;
-
-	while (ops[i].op)
-	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
-}
+int (*get_op_func(char *s))(int, int) 
+ { 
+         op_t op_s[] = { 
+                 {"+", op_add}, 
+                 {"-", op_sub}, 
+                 {"*", op_mul}, 
+                 {"/", op_div}, 
+                 {"%", op_mod}, 
+                 {NULL, NULL} 
+         }; 
+  
+         int i = 0; 
+  
+         while (op_s[i].op) 
+         { 
+                 if (*(op_s[i].op) == *s) 
+                         return (op_s[i].f); 
+                 i++; 
+         } 
+         return (NULL); 
+ }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,34 +11,37 @@
 *
 * Return: 0 success, 98 for argc error, 100 if Div 0
 */
-
-int main (int argc, char *argv[])
-{
-	int x = 0, y = 0, res = 0;
-	char s;
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	/* check if only one opearnd*/
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n")
-			exit(99);
-	}
-
-	s = argv[2][0];
-
-	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	x = atoi(argv[1]);
-	y = atoi(argv[3]);
-	res = (get_op_func(argv[2]))(a, b);
-	printf("%d\n", res);
-	return (0);
-}
+int main(int argc, char *argv[]) 
+ { 
+         int one, two, ans; 
+         int (*res)(int, int); 
+         char *get_op; 
+  
+         if (argc != 4) 
+         { 
+                 printf("Error\n"); 
+                 exit(98); 
+         } 
+  
+         one = atoi(argv[1]); 
+         two = atoi(argv[3]); 
+         get_op = argv[2]; 
+  
+         if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0') 
+         { 
+                 printf("Error\n"); 
+                 exit(99); 
+         } 
+  
+         if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0')) 
+         { 
+                 printf("Error\n"); 
+                 exit(100); 
+         } 
+  
+         res = get_op_func(get_op); 
+         ans = res(one, two); 
+  
+         printf("%d\n", ans); 
+         return (0); 
+ }
