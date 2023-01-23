53c9a43969245f94a88ae631c6bbf21c3a8d933e
AbiollaghJames <abiollajames@protonmail.com>
2023-01-23 05:26:03 -0800

    Task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,9 +1,12 @@
 #ifndef CALC_H
 #define CALC_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op-sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 /**
  * struct op - Struct op
@@ -16,12 +19,5 @@ typedef struct op
     char *op;
     int (*f)(int a, int b);
 } op_t;
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,27 +1,29 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * get_op_func - function pointer to select the right function
- * @s: operator given by user
- * Return: 0 Success
+ * @s: operator given by user * Return: 0 Success
  */
-int (*get_op_func(char *s))(int, int) 
- { 
-         op_t op_s[] = { 
-                 {"+", op_add}, 
-                 {"-", op_sub}, 
-                 {"*", op_mul}, 
-                 {"/", op_div}, 
-                 {"%", op_mod}, 
-                 {NULL, NULL} 
-         }; 
-  
-         int i = 0; 
-  
-         while (op_s[i].op) 
-         { 
-                 if (*(op_s[i].op) == *s) 
-                         return (op_s[i].f); 
-                 i++; 
-         } 
-         return (NULL); 
- }
+int (*get_op_func(char *s))(int, int)
+{
+	op_t op_s[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (op_s[i].op)
+	{
+		if (*(op_s[i].op == *s)
+				return (op_s[i].f);
+
+		i++;
+	}
+	return (NULL);
+}
