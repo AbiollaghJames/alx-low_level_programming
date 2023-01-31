044d2b5e389920ce091e194dbfaefcf59119c0af
AbiollaghJames <abiollajames@protonmail.com>
2023-01-31 12:52:48 -0800

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees struct list
+ * @head: node head
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
