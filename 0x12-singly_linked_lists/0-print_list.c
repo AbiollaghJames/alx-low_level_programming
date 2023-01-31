ea0dc4abe130b520d7af17df91240f3fb8c9a9bd
AbiollaghJames <abiollajames@protonmail.com>
2023-01-31 12:18:57 -0800

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - prints elements of list_t
+ * @h: parameter
+ * Return: Number of node
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h !- NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
