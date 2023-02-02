5b893bd8eeef4d886ae891c9046ec714fe8655c1
AbiollaghJames <abiollajames@protonmail.com>
2023-02-02 01:01:58 -0800

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,18 @@
 #include "lists.h"
 /**
  * print_listint - prints all elements of listint_t
- * @h: pointer to
- * Return: Number of nodes
+ * @h: pointer to firts node
+ * Return: Number of elements
  */
 size_t print_listint(const listint_t *h)
 {
-	unsigned int count = 0;
+	size_t elems;
+
+	while (h)
+	{
+		printf("%d", h->n);
+		elems++;
+		h = h->next;
+	}
+	return (elems);
 }
