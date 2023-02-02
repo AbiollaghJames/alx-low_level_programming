062b9f99deef6ea447eb51c878b8d4c293432e82
AbiollaghJames <abiollajames@protonmail.com>
2023-02-02 02:09:57 -0800

    Task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_nodeint - adds noe at beginning
+ * @head: pointer to head
+ * @n: data of node
+ * Return: address of new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *ptr = malloc(sizeof(listint_t));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	ptr->n = n;
+	ptr->next = head;
+	head = ptr;
+
+	return (*head);
+}
