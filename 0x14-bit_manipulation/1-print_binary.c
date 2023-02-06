36810bf3fd657f2638f256bf38feaca5b74e178a
AbiollaghJames <abiollajames@protonmail.com>
2023-02-06 12:20:40 -0800

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_binary - prints bin
+ * @n: unsigned int
+ * Return: bin
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
