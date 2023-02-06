8a816f746d95860472d2cb8c2181fcc1099919f3
AbiollaghJames <abiollajames@protonmail.com>
2023-02-06 10:18:26 -0800

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert binary to number
+ * @b: string representation of 0s and 1s
+ * Return: Unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int slen = strlen(b);
+	int total = 0;
+	int i;
+	unsigned int decval;
+
+	for (i = (slen - 1); i >= 0; i--)
+	{
+		if (b[i] == '1')
+		{
+			total += decval;
+			decval *= 2;
+		}
+	}
+	return (total);
+}
