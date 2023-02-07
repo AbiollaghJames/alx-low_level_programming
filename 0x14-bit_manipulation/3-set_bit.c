148e9a45007bf93d1b938ba2e5c0f1f8747d2afa
AbiollaghJames <abiollajames@protonmail.com>
2023-02-07 06:37:28 -0800

    task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * set_bit - sets value of a bit to 1 at given index
+ * @n: pointer to a number
+ * @index: index
+ * Return: 1 success or -1 error
+ */
+int set_bit(unsigned int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+
+	if (!((*n >> index) & 1))
+	{
+		*n += 1 << index;
+		return (1);
+	}
+	return (-1);
+}
