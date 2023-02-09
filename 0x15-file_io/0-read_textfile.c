26eaa80a215b6437328dac660359960d8023a6e1
AbiollaghJames <abiollajames@protonmail.com>
2023-02-08 18:05:04 -0800

    header file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * read_textfile - reads a file
+ * @filename: name of file
+ * @letters: number of letters read
+ * Return: number of letters read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <dirent.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
