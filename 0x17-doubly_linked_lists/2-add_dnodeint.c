aa74f97590426f69a18b93d32acb67a80e82bfa8
AbiollaghJames <abiollajames@protonmail.com>
2023-03-16 13:33:25 -0700

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->next = *head;
 	new_node->prev = NULL;
 	new_node->n = n;
-	*head = new_node;
 
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	
+	*head = new_node;
 	return (new_node);
 }
