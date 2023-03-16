6a5a0bb237b97f533efab03080cfbe9c74cc1ac1
AbiollaghJames <abiollajames@protonmail.com>
2023-03-16 14:17:32 -0700

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_dlistsint - function
+ * @head: pointer to first node
+ * Return: 0 on sucess
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *trav_ptr = head;
+
+	if (head == NULL)
+		return;
+
+	while (head->next != NULL)
+	{
+		trav_ptr = head;
+		head = head->next;
+		free(trav_ptr);
+	}
+	free(head);
+}
