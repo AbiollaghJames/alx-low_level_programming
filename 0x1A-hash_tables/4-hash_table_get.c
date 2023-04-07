75f04d1a1b28b811e2187bf732106beacbd2b5f5
AbiollaghJames <abiollajames@protonmail.com>
2023-04-07 09:20:04 -0700

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp;
 
-	if (!key)
+	if (!key || strlen(key) == 0)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
