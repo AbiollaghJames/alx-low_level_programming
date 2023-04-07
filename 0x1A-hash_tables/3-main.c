20fb66fc0667b55a88161a39ae38791cf1cdaa84
AbiollaghJames <abiollajames@protonmail.com>
2023-04-07 09:18:21 -0700

    task 4

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -13,6 +13,6 @@ int main(void)
     hash_table_t *ht;
 
     ht = hash_table_create(1024);
-    hash_table_set(ht, "betty", "cool");
+    hash_table_set(ht, "hetairas", "mentioner");
     return (EXIT_SUCCESS);
 }
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves value of associated key
+ * @ht: hash tables to be worked upon
+ * @key: key to retriev its value
+ * Return: value of key or NULL if there is no key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (!key)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			return (temp->value);
+		}
+		temp = temp->next;
+	}
+	return (0);
+}
