2ccf9e3cca921b56ef447e42760a6236e2d9e219
AbiollaghJames <abiollajames@protonmail.com>
2023-04-07 02:19:35 -0700

    task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,8 +8,8 @@ unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash = 5381;
 	int c;
-
-	while((c = *str++))
+	
+	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c;
 	}
