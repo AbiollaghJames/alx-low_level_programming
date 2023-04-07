459d69b0e07e8390cbb4f733618c6e93d7a219ae
AbiollaghJames <abiollajames@protonmail.com>
2023-04-07 01:07:42 -0700

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: size of the hash table
+ * Return: newly created hash tbale
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t* table = (hash_table_t*)malloc(sizeof(hash_table_t));
+
+	if (!table)
+		return NULL;
+
+	table->size = size;
+	table->array = (hash_node_t**)calloc(table->size, sizeof(hash_node_t*));
+
+	return (table);
+}
