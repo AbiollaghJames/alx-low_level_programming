9c57698f4e1c50c1f37c7d7a5b848bd74908e4ae
AbiollaghJames <abiollajames@protonmail.com>
2023-04-07 01:13:21 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,13 +6,13 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t* table = (hash_table_t*)malloc(sizeof(hash_table_t));
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (!table)
-		return NULL;
+		return (NULL);
 
 	table->size = size;
-	table->array = (hash_node_t**)calloc(table->size, sizeof(hash_node_t*));
+	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 
 	return (table);
 }
