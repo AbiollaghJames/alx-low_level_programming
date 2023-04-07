b01b7d3352c06f01556d8c7120fded4219133a4f
AbiollaghJames <abiollajames@protonmail.com>
2023-04-07 02:17:06 -0700

    task 1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - computes the hash of char array str
+ * @str: string to compute it's hash value
+ * Return: hash of the string str
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
