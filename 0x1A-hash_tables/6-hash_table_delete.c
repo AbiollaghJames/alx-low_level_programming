6c95c3b5f2caca27b668bd797195936a61cf0051
AbiollaghJames <abiollajames@protonmail.com>
2023-04-07 10:04:54 -0700

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to be deleted
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *node, *temp;
+
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+
+		while (node != NULL)
+		{
+			temp = node;
+			node = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
