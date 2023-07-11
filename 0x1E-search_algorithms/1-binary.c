a89800868c7ddb278b7d1a76480204a99521a967
AbiollaghJames <abiollajames@protonmail.com>
2023-07-11 12:57:06 -0700

    Binary Search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -35,6 +35,9 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t low = 0, high = size - 1;
 
+	if (array == NULL)
+		return (-1);
+
 	while (low <= high)
 	{
 		size_t mid = low + (high - low) / 2;
